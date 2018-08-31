84085c7cd0a9cd19be9a0b4ea2b53d5e5d7d1593
nemuu <uk39.nemunemu@gmail.com>
2018-08-31 20:31:27 +0900

    structure develop env

--- pdca-manager-desktop/gulpfile.js
+++ pdca-manager-desktop/gulpfile.js
@@ -1,6 +1,7 @@
 // src -> dist
 var gulp = require('gulp');
- var typescript = require('gulp-typescript');
+var typescript = require('gulp-typescript');
+var plumber = require('gulp-plumber');
 
 gulp.task('htmlTransfer', function(){
   gulp.src('./src/**/*.html')
@@ -12,8 +13,15 @@ gulp.task('tsCompiler', function(){
          './src/**/*.ts',
          '!./node_modules/**'//node_modules配下は除外する
        ])
+      .pipe(plumber())
       .pipe(typescript())
       .pipe(gulp.dest('./dist'));
   });
 
-gulp.task('default', ['htmlTransfer', 'tsCompiler']);
+// 監視を行う
+gulp.task('watch', function() {
+    gulp.watch('./src/**/*.ts', ['tsCompiler'])
+    gulp.watch('./src/**/*.html', ['htmlTransfer'])
+});
+
+gulp.task('default', ['htmlTransfer', 'tsCompiler', 'watch']);
--- pdca-manager-desktop/src/main.ts
+++ pdca-manager-desktop/src/main.ts
@@ -1,5 +1,3 @@
-/// <reference path="../typings/index.d.ts"/>
-
 // In the main process.
 const { app, BrowserWindow } = require('electron')
 
@@ -7,8 +5,20 @@ app.on('window-all-closed', () => {
     app.quit()
 })
 
+let mainWindow;
 app.on('ready', () => {
-    let win = new BrowserWindow({ width: 800, height: 600 })
-    // load a local HTML file
-    win.loadURL(`file:///index.html`)
+  // アプリケーションメニューの構成
+  // Menu.setApplicationMenu(mainMenu);
+  // メイン画面起動
+  mainWindow = new BrowserWindow({width: 600, height: 400});
+  mainWindow.loadURL('file://' + __dirname + '/index.html');
+  console.log("起動確認");
+
+  // デバッグ用
+  mainWindow.webContents.openDevTools();
+
+  // 終了時のオプション
+  mainWindow.on('closed', function() {
+    mainWindow = null;
+  });
 })
